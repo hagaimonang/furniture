683d2b61feaeba72ae268e61de5e41dce20c9a8d
hagaimonang <hagaimc15@gmail.com>
2025-10-30 16:41:43 +0700

    Upload website furniture

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById("menuBtn");
+const mainNav = document.getElementById("mainNav");
+
+menuBtn.addEventListener("click", () => {
+  mainNav.classList.toggle("active");
+});
